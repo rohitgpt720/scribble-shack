2a9170620b4afae73e74ec0afd3cfc6b7accec15
rohitgpt720 <rohitgpt720@gmail.com>
2024-02-26 21:01:24 +0530

    changed image domain in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  image: {
+    domains: ['https://rohitgpt720.kinde.com/'],
+},
+};
 
 export default nextConfig;
